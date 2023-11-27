92d2e6437eeb60c38d977635ee90a0cbe187bb55
mohamed <mohamedchebbbi@gmail.com>
2023-11-27 21:01:02 +0100

    more

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -2,21 +2,13 @@
 /* eslint-disable indent */
 /* eslint-disable linebreak-style */
 export default function getSanFranciscoDescription() {
-    // eslint-disable-next-line indent
-    // eslint-disable-next-line linebreak-style
-    // eslint-disable-next-line indent
-    const year = 2017;
-    // eslint-disable-next-line indent
-    const budget = {
-      income: '$119,868',
-      gdp: '$154.2 billion',
-      capita: '$178,479',
-    // eslint-disable-next-line linebreak-style
-    };
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
 
-    // eslint-disable-next-line indent
-    return `As of ${year}, it was the seventh-highest income county in the United States\
-  , with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
-  proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
-  // eslint-disable-next-line eol-last
-  }
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+// eslint-disable-next-line eol-last
+}
--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,13 +1,13 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
 /* eslint-disable linebreak-style */
-// eslint-disable-next-line linebreak-style
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-      income,
-      gdp,
-      capita,
-    };
-    return budget;
-  // eslint-disable-next-line eol-last
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+// eslint-disable-next-line eol-last
 }
