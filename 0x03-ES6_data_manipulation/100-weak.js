7253bd21556fc20319e051f860cff0d96c94ad47
mohamed <mohamedchebbbi@gmail.com>
2023-12-04 22:11:32 +0100

    advanced

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+export const weakMap = new WeakMap();
+let count = 1;
+
+export function queryAPI(endPoint) {
+  weakMap.set(endPoint, count);
+  count += 1;
+  const queryCount = weakMap.get(endPoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
